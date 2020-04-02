2b453d37b9a275035da71f70844d11b985b7a338
IMBERT <muriel.imbert05@gmail.com>
2020-04-02 19:28:51 +0200

    Commit initial projet vide avec le plugin builder

---
+++ storage/framework/classes.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins/october/demo/Plugin.php',
+  'backend\\Controllers\\auth' => 'modules/backend/controllers/auth.php',
+  'backend\\Controllers\\index' => 'modules/backend/controllers/index.php',
+  'system\\Controllers\\settings' => 'modules/system/controllers/settings.php',
+  'system\\Controllers\\updates' => 'modules/system/controllers/updates.php',
+  'rainlab\\builder\\Plugin' => 'plugins/rainlab/builder/Plugin.php',
+);
