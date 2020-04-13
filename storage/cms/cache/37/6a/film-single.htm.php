90d07ebf45fcba8cdb7704ee422863e243d83aa3
IMBERT <muriel.imbert05@gmail.com>
2020-04-13 10:34:58 +0200

    Modif .gitignore ainsi que debug  url pour l'affichage d'un film

--- storage/cms/cache/37/6a/film-single.htm.php
+++ storage/cms/cache/37/6a/film-single.htm.php
@@ -1,5 +1,5 @@
 <?php 
-class Cms5e941cd97e178470508093_19310a2e2e9359d405b3ab1be7c1e254Class extends Cms\Classes\PageCode
+class Cms5e94236dbb9ba248713105_0f6c465812bca24710febe7bb3dcefc4Class extends Cms\Classes\PageCode
 {
 
 }
